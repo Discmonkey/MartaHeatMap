1ccb672cdbdc4ce4ea9db5a223c51d6d5f3ae8e8
maxg <frismo98@gmail.com>
2016-10-29 18:35:12 -0400

    + spinner

--- MartaHeatMap.py
+++ MartaHeatMap.py
@@ -2,20 +2,21 @@ from flask import Flask, render_template, request
 from sources.gmaps import DirectionsApi
 import json
 import os
-
+import os
+file_path = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
 def get_cache(lat, lng, length):
     file_name = str(lat) + str(lng) + str(length)
-    if os.path.isfile('/Users/maxg/PycharmProjects/MartaHeatMap/cache/' + file_name):
-        f = open('/Users/maxg/PycharmProjects/MartaHeatMap/cache/' + file_name)
+    if os.path.isfile(file_path + '/cache/' + file_name):
+        f = open(file_path + '/cache/' + file_name)
         return json.loads(f.read())
     else:
         return False
 
 def write_cache(lat, lng, length, object):
     file_name = str(lat) + str(lng) + str(length)
-    f = open('/Users/maxg/PycharmProjects/MartaHeatMap/cache/' + file_name, 'w')
+    f = open(file_path + '/cache/' + file_name, 'w')
     f.write(json.dumps(object))
 
 @app.route('/')
