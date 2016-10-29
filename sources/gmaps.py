e259f9dbae08e8bb84206a36aeec419c8bd9bd5a
Jonathan Lian <jonathan.hahaha@gmail.com>
2016-10-29 09:22:04 -0400

    changed api key

--- sources/gmaps.py
+++ sources/gmaps.py
@@ -1,10 +1,14 @@
 import requests
 import json
 
-api_key = 'AIzaSyB6V_cnPLawq06s1_O_1fFYNXB6bqAeunE'
+# api_key = 'AIzaSyB6V_cnPLawq06s1_O_1fFYNXB6bqAeunE'
+# replaced with Jonathan's API key
+api_key = 'AIzaSyA99KgXUFhvyxIbiX_sDprmN9PGYwKbazQ'
+
 
 class DirectionsApi:
-    api_key = 'AIzaSyB6V_cnPLawq06s1_O_1fFYNXB6bqAeunE'
+    # api_key = 'AIzaSyB6V_cnPLawq06s1_O_1fFYNXB6bqAeunE'
+    api_key = 'AIzaSyA99KgXUFhvyxIbiX_sDprmN9PGYwKbazQ'
 
     def make_request(self, lat1, long1, lat2, long2, key=None):
 
@@ -44,6 +48,10 @@ def find_surrounding(input_coord, length, mile_increment):
         mean points are 0.5 miles away from each other in the area
     
     """
+    f = open("coordinates.txt", "w")
+    data = "["
+    
+    
     d = DirectionsApi()
     center = ['33.7490', '-84.3880']
     
@@ -59,16 +67,24 @@ def find_surrounding(input_coord, length, mile_increment):
             all_coords.append((round(coord_x, 4), round(coord_y, 4)))
             
             """If you want to get duration as well, uncomment below. Above is for testing"""
-            # try:
+            try:
                 # print(d.parse(d.make_request(input_coord[0], input_coord[1], (str) (coord_x), (str) (coord_y))))
-            # except:
-                # print("No transit data")
+                #duration = d.parse(d.make_request(input_coord[0], input_coord[1], (str) (coord_x), (str) (coord_y)))
+                data += "{{lat: {}, lng: {}, count: {}}},".format((coord_x), (coord_y), (d.parse(d.make_request(input_coord[0], input_coord[1], (str) (coord_x), (str) (coord_y)))))
+            except:
+                print("No transit data")
+                
             
-            # all_coords.append((round(top_left_x, 4), round(top_left_y, 4), duration))
             
+            # all_coords.append((str) (coord_x), (str) (coord_y), duration))
+            
+    data += "]"
+    f.write(data)
+    f.close()
+    
     print(all_coords)       
     
-# find_surrounding(input_coord = ['33.7490', '-84.3880'], length = 5, mile_increment = 0.5) # HARDCODED FOR NOW, read in data later
-find_surrounding(input_coord = ['33.7490', '-84.3880'], length = 20, mile_increment = 2) # HARDCODED FOR NOW, read in data later
+find_surrounding(input_coord = ['33.7490', '-84.3880'], length = 3, mile_increment = 0.5) # HARDCODED FOR NOW, read in data later
+#find_surrounding(input_coord = ['33.7490', '-84.3880'], length = 20, mile_increment = 2) # HARDCODED FOR NOW, read in data later
     
     
